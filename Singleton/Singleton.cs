891d2648deab31668a84e5b7db57b110f76541ef
Tomasz Kaczmarz <tkaczmarz@INF.UG.EDU.PL>
2019-03-01 15:57:32 +0100

    Added singleton implementation.

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -4,6 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Singleton
+[Serializable]
+public sealed class Singleton
 {
+    private static Singleton instance = null;
+    private static readonly object padlock = new object();
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            }
+
+            return instance;
+        }
+    }
 }
